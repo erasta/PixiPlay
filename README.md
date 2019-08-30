26394be3f20e96153837cb79aba1e975571d95c3
Eran Geva <eran.geva@gmail.com>
2019-08-31 01:36:58 +0200

    full screen

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PixiPlay
 Pixi graphics play in JS
+
+https://erasta.github.io/PixiPlay/dist/index.html
--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,51 @@
 import { Application, loader } from 'pixi.js';
 import { Character } from '@app/character.class';
 class Game {
-  private app: Application;
-  constructor() {
-    // instantiate app
-    this.app = new Application({
-      width: 800,
-      height: 600,
-      backgroundColor: 0x1099bb // light blue
-    });
+    private app: Application;
+    constructor() {
+        // instantiate app
+        this.app = new Application({
+            width: 800,
+            height: 600,
+            backgroundColor: 0x1099bb // light blue
+        });
 
-    // create view in DOM
-    document.body.appendChild(this.app.view);
+        // create view in DOM
+        document.body.appendChild(this.app.view);
 
-    // preload needed assets
-    loader.add('samir', '/assets/img/hero.png');
+        this.app.renderer.view.style.top = "0px";
+        this.app.renderer.view.style.left = "0px";
+        this.app.renderer.view.style.position = "absolute";
+        this.app.renderer.view.style.display = "block";
+        this.app.renderer.autoResize = true;
+        this.app.renderer.resize(window.innerWidth, window.innerHeight);
 
-    // then launch app
-    loader.load(this.setup.bind(this));
-  }
+        // preload needed assets
+        loader.add('samir', '/assets/img/hero.png');
 
-  setup(): void {
-    // append hero
-    const hero = new Character(loader.resources['samir'].texture);
-    const heroSprite = hero.sprite;
-    this.app.stage.addChild(heroSprite);
-    heroSprite.y = 300;
+        // then launch app
+        loader.load(this.setup.bind(this));
+    }
 
-    //  animate hero
-    let moveLeft = true;
-    this.app.ticker.add(() => {
-      const speed = 2;
-      if (heroSprite.x < this.app.view.width && moveLeft) {
-        heroSprite.x += speed;
-      } else {
-        heroSprite.x -= speed;
-        moveLeft = heroSprite.x <= 0;
-      }
-    });
-  }
+    setup(): void {
+        // append hero
+        const hero = new Character(loader.resources['samir'].texture);
+        const heroSprite = hero.sprite;
+        this.app.stage.addChild(heroSprite);
+        heroSprite.y = 300;
+
+        //  animate hero
+        let moveLeft = true;
+        this.app.ticker.add(() => {
+            const speed = 2;
+            if (heroSprite.x < this.app.view.width && moveLeft) {
+                heroSprite.x += speed;
+            } else {
+                heroSprite.x -= speed;
+                moveLeft = heroSprite.x <= 0;
+            }
+        });
+    }
 }
 
 new Game();
